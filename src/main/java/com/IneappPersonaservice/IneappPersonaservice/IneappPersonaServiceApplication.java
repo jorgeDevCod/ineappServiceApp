474301b951cac58f2a8c95487415c9f8fa3a1e12
jorgeDevCod <jorge2698.jgpm@gmail.com>
2022-09-15 21:16:43 -0500

    Iniciando proyecto

---
+++ src/main/java/com/IneappPersonaservice/IneappPersonaservice/IneappPersonaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.IneappPersonaservice.IneappPersonaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IneappPersonaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IneappPersonaServiceApplication.class, args);
+	}
+
+}
