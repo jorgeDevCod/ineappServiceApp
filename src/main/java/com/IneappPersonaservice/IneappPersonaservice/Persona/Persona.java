0219e8e59b2faf71d0908273d4f328dd685204fd
jorgeDevCod <jorge2698.jgpm@gmail.com>
2022-09-25 00:02:01 -0500

    Clase 4 subida a git

--- src/main/java/com/IneappPersonaservice/IneappPersonaservice/Persona/Persona.java
+++ src/main/java/com/IneappPersonaservice/IneappPersonaservice/Persona/Persona.java
@@ -1,13 +1,40 @@
 package com.IneappPersonaservice.IneappPersonaservice.Persona;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
-@AllArgsConstructor
-@NoArgsConstructor
-@Data
-public class Persona {
+import javax.persistence.*;
+import java.time.LocalDate;
 
+@Entity(name ="Persona")
+@Table(
+        name ="Persona",
+        uniqueConstraints = {
+                @UniqueConstraint(name = "persona_email_unique", columnNames = "email"),
+                @UniqueConstraint(name = "persona_dni_unique", columnNames = "dni"),
+            }
+        )
+
+@Getter
+@Setter
+@Builder
+public class Persona{
+
+    @SequenceGenerator(
+            name = "persona_sequence",
+            sequenceName = "persona_sequence",
+            allocationSize = 1
+    )
+
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "persona_sequence"
+    )
+    @Id
+    Long id;
     String nombre;
+    String apellidos;
+    String dni;
+    String email;
+    LocalDate birthDate;
+
 }
--- src/main/java/com/IneappPersonaservice/IneappPersonaservice/Persona/PersonaController.java
+++ src/main/java/com/IneappPersonaservice/IneappPersonaservice/Persona/PersonaController.java
@@ -4,18 +4,45 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
 @RestController
 @RequestMapping("/persona")
 public class PersonaController {
 
     @GetMapping
     @RequestMapping("/getAll")
+    public List<Persona> getAll(){
 
-    public String getPersona(){
-        Persona persona = new Persona ("Jorge");
-        Persona persona1 = new Persona();
+        LocalDate date = LocalDate.of(1994, Month.SEPTEMBER, 5);
 
-        return persona.getNombre();
+        Persona persona = Persona.builder()
+                .id(1l)
+                .nombre("German")
+                .apellidos("Mesta")
+                .dni("75116689")
+                .email("Jgmesta@gmail.com")
+                .birthDate(date).build();
+
+        return List.of(persona);
+    }
+
+
+    @GetMapping
+    @RequestMapping("/getbyid")
+    public Persona getById(){
+        LocalDate date = LocalDate.of(1999, Month.SEPTEMBER, 5);
+
+        Persona persona = Persona.builder()
+                .id(1l)
+                .nombre("Jorge")
+                .apellidos("Pasapera")
+                .dni("75116789")
+                .email("Jmesta@gmail.com")
+                .birthDate(date).build();
+        return persona;
     }
 
     //Put
